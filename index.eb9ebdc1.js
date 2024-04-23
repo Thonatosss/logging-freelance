fb644138890a00a69138d0ba17c566f280fd8711
Thonatosss <sven4369@gmail.com>
2024-04-23 18:52:10 +0000

    Deploying to gh-pages from @ Thonatosss/logging-freelance@aed2ee4509a1af4f17a69a7e47099606def9eb23 🚀

---
+++ index.eb9ebdc1.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",(function(){var e=document.getElementById("menu__toggle"),n=document.getElementById("menu__overlay"),t=document.querySelectorAll(".menu__item");e.addEventListener("change",(function(){document.body.classList.toggle("menu-open",e.checked)})),n.addEventListener("click",(function(){e.checked=!1,document.body.classList.remove("menu-open")})),t.forEach((function(n){n.addEventListener("click",(function(){e.checked=!1,document.body.classList.remove("menu-open")}))}))}));
+//# sourceMappingURL=index.eb9ebdc1.js.map
