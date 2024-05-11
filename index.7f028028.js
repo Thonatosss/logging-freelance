4654a107d69301bce1bccbf547ae3d6a8e3850ac
Thonatosss <sven4369@gmail.com>
2024-05-11 23:08:42 +0000

    Deploying to gh-pages from @ Thonatosss/logging-freelance@f71d0c7ddebbef3ee620419b61304089851533ab 🚀

---
+++ index.7f028028.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",(function(){var e=["./src/images/hero/heroImg.jpg","./src/images/hero/hero-second.jpg"],n=0,o=document.querySelector(".hero-section");setInterval((function(){n=(n+1)%e.length,setTimeout((function(){o.style.backgroundImage="url('"+e[n]+"')"}),1e3)}),5e3)}));
+//# sourceMappingURL=index.7f028028.js.map
